7b6e9a3b1793285e5ba3bc349e714d66f3ec26e7
ser1zw <azariahsawtikes@gmail.com>
2021-02-17 03:23:29 +0900

    small fix

--- client/store/auth.js
+++ client/store/auth.js
@@ -3,7 +3,7 @@ export const state = () => ({
 })
 
 export const mutations = {
-  setLoggedInStatus(state, isLoggedIn) {
+  setLoginStatus(state, isLoggedIn) {
     state.isLoggedIn = isLoggedIn
   }
 }
@@ -11,7 +11,7 @@ export const mutations = {
 export const actions = {
   async isLoggedIn({ commit }) {
     let response = await this.$axios.$get('http://localhost:8080/api/loginstatus')
-    commit('setLoggedInStatus', response.isLoggedIn)
+    commit('setLoginStatus', response.isLoggedIn)
     return response.isLoggedIn
   },
   async login({ commit }, { loginId, password }) {
@@ -19,11 +19,11 @@ export const actions = {
     params.append('username', loginId)
     params.append('password', password)
     let response = await this.$axios.$post('http://localhost:8080/api/login', params)
-    commit('setLoggedInStatus', true)
+    commit('setLoginStatus', true)
     return response
   },
   async logout({ commit }) {
     await this.$axios.$post('http://localhost:8080/api/logout')
-    commit('setLoggedInStatus', false)
+    commit('setLoginStatus', false)
   }
 }
